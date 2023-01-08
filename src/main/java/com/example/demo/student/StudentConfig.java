6e2ca9b43b650134cd589fd874f9d7f4cd8e558c
fyupanquia <fyupanquia@outlook.com>
2023-01-08 11:42:36 -0500

    student restful

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return  args -> {
+            Student mariam = new Student( "Mariam", "mariam@example.com", LocalDate.of(2000, Month.JANUARY, 3));
+            Student alex = new Student("Alex", "alex@example.com", LocalDate.of(2001, Month.JANUARY, 3));
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+}
